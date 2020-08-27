696d3f5f5398b47faf222eac1656ea4fc9e57fc7
Christian Dupuis <cd@atomist.com>
2020-08-27 20:06:52 +0200

    Only once per day

--- skill.ts
+++ skill.ts
@@ -88,7 +88,7 @@ export const Skill = skill<
 		schedule: {
 			type: ParameterType.Schedule,
 			displayName: "Process branches",
-			defaultValue: "0 */6 * * *",
+			defaultValue: "0 8 * * *",
 			description: "Cron expression to process stale branches",
 			required: false,
 			visibility: ParameterVisibility.Hidden,
